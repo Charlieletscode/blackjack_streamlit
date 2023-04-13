082f7560cafd6b979e985311cdb45f465929f221
Charlieletscode <37406313+Charlieletscode@users.noreply.github.com>
2023-04-12 21:47:44 -0400

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -7,8 +7,8 @@ card_images = {}
 
 # Define the card deck
 suits = ['hearts', 'diamonds', 'clubs', 'spades']
-card_deck = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
-card_values = {'Ace': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'Jack': 10, 'Queen': 10, 'King': 10}
+card_deck = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
+card_values = {'ace': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'jack': 10, 'queen': 10, 'king': 10}
 
 for suit in suits:
     for rank in card_deck:
